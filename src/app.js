9d9b2ae8cedfaaa9f594a791c8ceb7d71e51bb3c
roseshay <165499006+roseshay@users.noreply.github.com>
2024-04-21 15:29:07 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("btn").onclick = function() {
+    let person = ["my cat", "my daughter", "my dog", "an alien"];
+    let action = ["play outside", "fell in water", "destroyed my ", "stole my"];
+    let posession = ["car", "phone", "house", "clothes", "laptop"];
+    let time = [
+      "before i could leave",
+      "when i walk outside",
+      "on my way to class",
+      "on my way to work"
+    ];
+
+    let randomIdx = Math.floor(Math.random() * person.length);
+    let perIndxR = person[randomIdx];
+    let actIndxR = action[Math.floor(Math.random() * action.length)];
+    let posIndexR = posession[Math.floor(Math.random() * posession.length)];
+    let timIndexR = time[Math.floor(Math.random() * time.length)];
+
+    document.getElementById(
+      "excuse"
+    ).innerHTML = `${perIndxR} ${actIndxR} ${posIndexR} ${timIndexR}`;
+  };
 };
